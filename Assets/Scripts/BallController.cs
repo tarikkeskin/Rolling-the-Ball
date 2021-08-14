a22cbff2b94276bdf796c30452a94ae5e16c0d5a
Tarık <60899740+tarikkeskin@users.noreply.github.com>
2021-08-15 01:12:10 +0300

    RollTheBall
    Game Done

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    Rigidbody fiz;
+    public int hiz=1;
+    int score = 0;
+    public int allBalls;
+    public Text scoreText;
+    public Text gameOverText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fiz = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(hor);
+        Debug.Log(ver);
+
+        Vector3 vec = new Vector3(hor,0,ver);
+
+        fiz.AddForce(vec*hiz);
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "obstacle")
+        {
+            other.gameObject.SetActive(false);
+            //Destroy(other.gameObject);
+
+            score++;
+
+        }
+
+        scoreText.text = "Score : " + score;
+
+        if (score == allBalls) {
+
+            gameOverText.text = "Game Over";
+           // Debug.Log("Game Finished!");
+        }
+    }
+}
---
+++ Assets/Scripts/cameracontrol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontrol : MonoBehaviour
+{
+
+    public GameObject  ball;
+
+    Vector3 distance;
+    
+
+    void Start()
+    {
+        distance = transform.position - ball.transform.position;
+    
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + distance;
+    }
+}
